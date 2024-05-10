d9159d678507590f1cd4288f5e35918748e9b9f0
monwar23 <monwar2345@gmail.com>
2024-05-11 00:43:46 +0600

    usehook created

---
+++ src/firebase/AuthProvider.jsx
@@ -0,0 +1,76 @@
+import { GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import app from "./firebase.config";
+
+export const AuthContext=createContext(null)
+const auth=getAuth(app)
+
+const googleProvider = new GoogleAuthProvider()
+const gitHubProvider = new GithubAuthProvider()
+
+const FirebaseProvider = ({children}) => {
+
+    const [user,setUser]=useState(null)
+    const [loading,setLoading]=useState(true)
+
+    // create user
+
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+     // sign in
+     const signIn = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+
+     //   google
+     const googleLogin = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    };
+    //   github
+    const gitHubLogin = () => {
+        setLoading(true);
+        return signInWithPopup(auth, gitHubProvider);
+    };
+
+    const logOut = () => {
+        setLoading(true)
+        return signOut(auth)
+    }
+
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            setLoading(false);
+        })
+        return () => {
+            unSubscribe()
+        }
+    }, [])
+
+    const authInfo={
+        user,
+        setUser,
+        loading,
+        createUser,
+        signIn,
+        googleLogin,
+        gitHubLogin,
+        logOut
+    }
+
+    return (
+        <div>
+            <AuthContext.Provider value={authInfo}>
+            {children}
+            </AuthContext.Provider>
+        </div>
+    );
+};
+
+export default FirebaseProvider;
