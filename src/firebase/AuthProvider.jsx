9f0899a928a80214a128f78ebb801ab614ec688f
monwar23 <monwar2345@gmail.com>
2024-05-11 00:48:59 +0600

    firebase Authprovider completed

--- src/firebase/AuthProvider.jsx
+++ src/firebase/AuthProvider.jsx
@@ -1,14 +1,14 @@
 import { GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
-import { createContext, useEffect, useState } from "react";
 import app from "./firebase.config";
+import { createContext, useEffect, useState } from "react";
+
 
 export const AuthContext=createContext(null)
 const auth=getAuth(app)
 
 const googleProvider = new GoogleAuthProvider()
 const gitHubProvider = new GithubAuthProvider()
-
-const FirebaseProvider = ({children}) => {
+const AuthProvider = ({children}) => {
 
     const [user,setUser]=useState(null)
     const [loading,setLoading]=useState(true)
@@ -63,14 +63,14 @@ const FirebaseProvider = ({children}) => {
         gitHubLogin,
         logOut
     }
-
     return (
         <div>
             <AuthContext.Provider value={authInfo}>
-            {children}
+                {children}
             </AuthContext.Provider>
+            
         </div>
     );
 };
 
-export default FirebaseProvider;
+export default AuthProvider;
