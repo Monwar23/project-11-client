d2736e17b20cc2171312ebdab8155f9a7e424a31
monwar23 <monwar2345@gmail.com>
2024-05-12 02:10:36 +0600

    6 data show in the home page by purchase count

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,16 +4,16 @@ import { useEffect, useState } from "react";
 import FoodCard from "../../components/FoodCard";
 
 const Home = () => {
-    const [foods, setFoods] = useState([])
+    const [foods, setFoods] = useState([]);
 
     useEffect(() => {
         fetch(`${import.meta.env.VITE_APP_URL}/foods`)
             .then(res => res.json())
-            .then(data=>{
-                const sortedFoods = data.sort((a, b) => b.purchaseCount - a.purchaseCount);
-                setFoods(sortedFoods)
-            })
-    }, [])
+            .then(data => {
+                const sortedFoods = data.sort((a, b) => b.purchase_count - a.purchase_count);
+                setFoods(sortedFoods);
+            });
+    }, []);
 
     return (
         <div>
@@ -21,9 +21,10 @@ const Home = () => {
                 <title>DineDash | Home</title>
             </Helmet>
             <Swipper></Swipper>
-            <div>
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 my-10">
                 {
-                    foods.slice(0, 6).map(food=> <FoodCard key={food} food={food}></FoodCard>)
+                    foods.slice(0,6).map(food => <FoodCard key={food._id} food={food}></FoodCard>)
+                    
                 }
             </div>
         </div>
