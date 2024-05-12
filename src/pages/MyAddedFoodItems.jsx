1fdf2b09c3f23f3cac174096742128e7bf3f5e73
monwar23 <monwar2345@gmail.com>
2024-05-13 03:39:34 +0600

    added sweet alart

--- src/pages/MyAddedFoodItems.jsx
+++ src/pages/MyAddedFoodItems.jsx
@@ -20,6 +20,38 @@ const MyAddedFoodItems = () => {
             });
     }, [user]);
 
+    const handleDelete = _id => {
+        Swal.fire({
+          title: "Are you sure?",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+          if (result.isConfirmed) {
+    
+    
+            fetch(`https://project-10-server-gray.vercel.app/craftSection/${_id}`,{
+              method:'DELETE'
+            })
+              .then(res => res.json())
+              .then(data => {
+                console.log(data);
+                if (data.deletedCount > 0) {
+                  Swal.fire({
+                    title: "Deleted!",
+                    text: "Your file has been deleted.",
+                    icon: "success"
+                  });
+                  setControl(!control)
+                }
+              })
+          }
+        });
+      }
+
     return (
         <div>
             <Helmet>
